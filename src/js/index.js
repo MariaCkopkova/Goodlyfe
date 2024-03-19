4682fb38220de45a48c67366e7278cdbd7a9b39c
MariaCkopkova <mariakons@mail.ru>
2024-03-19 14:12:31 +0500

    header-fix

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,29 @@ mobileNav();
 import searchForm from "./modules/search";
 searchForm();
 
+// Swiper 
+import Swiper from 'swiper';
+import { Navigation, Pagination, EffectFade } from 'swiper/modules';
+import '../../node_modules/swiper/swiper-bundle.min.css';
+//import '../../node_modules/swiper/modules/effect-fade.min.mjs';
+
+//import 'swiper/css/navigation';
+//import 'swiper/css/pagination';
+const swiper = new Swiper('.slider', {
+  modules: [Navigation, Pagination, EffectFade],
+  spaceBetween: 30,
+  loop: true,
+  speed: 1000,
+  effect: 'fade',
+  crossfade: true,
+  lazy: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
+});
 
--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -3,8 +3,8 @@ function mobileNav() {
         const mobileMeny = document.querySelector('#header-meny')
         const bodyEI = document.body;
         if (menyToggle) {
+                /**клик по иконке бургер */
                 menyToggle.addEventListener('click', () => {
-                        console.log("hi")
                         if (menyToggle.classList.contains('active')) {
                                 menyToggle.classList.remove('active');
                                 mobileMeny.classList.remove('active');
@@ -14,6 +14,12 @@ function mobileNav() {
                                 mobileMeny.classList.add('active');
                                 bodyEI.classList.add('no-scroll');
                         }
+                });
+                /**клик по мобильному меню */
+                mobileMeny.addEventListener('click', () => {
+                        menyToggle.classList.remove('active');
+                        mobileMeny.classList.remove('active');
+                        bodyEI.classList.remove('no-scroll');
                 });
         }
 }
